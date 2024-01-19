f0649970d603088047b3b8060678deafa454d31f
Ataulhaq Akbar <ataul.akbar@gmail.com>
2024-01-19 16:53:06 -0500

    writing the jsonToCsv script

--- index.mjs
+++ index.mjs
@@ -1,4 +1,22 @@
 
 import data from './sample_data/data.json' assert { type: 'json' };
 
-console.log(data);
+import Papa from "papaparse";
+
+// Convert JSON to CSV
+const csvData = Papa.unparse(data.Bars, {
+    header: true, // Include headers in the CSV file
+});
+  
+  // Create a Blob from the CSV data
+const blob = new Blob([csvData], { type: 'text/csv;charset=utf-8;' });
+  
+  // Create a download link
+const link = document.createElement('a');
+link.href = URL.createObjectURL(blob);
+link.setAttribute('download', 'output.csv');
+  
+  // Append the link to the body and trigger the download
+document.body.appendChild(link);
+link.click();
+document.body.removeChild(link);
