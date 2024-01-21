a3820a109a1be46be07cacec110d01d05349f2b0
Ataulhaq Akbar <ataul.akbar@gmail.com>
2024-01-21 13:36:54 -0500

    changing code to accept json payload

--- index.mjs
+++ index.mjs
@@ -1,5 +1,5 @@
 
-import data from './sample_data/data.json' assert { type: 'json' };
+//import data from './sample_data/data.json' assert { type: 'json' };
 import Papa from "papaparse";
 import express from 'express';
 import fs from 'fs';
@@ -8,21 +8,29 @@ const port = 3000;
 
 
 // Convert JSON to CSV
-const csvData = Papa.unparse(data.Bars, {
-    header: true, // Include headers in the CSV file
-});
+//const csvData = Papa.unparse(data.Bars, {
+//    header: true, // Include headers in the CSV file
+//});
 
 app.get('/download-csv', (req, res) => {
   
+    const data = req.body;
     // Set response headers for CSV file download
+
+    const csvData = Papa.unparse(jsonData, {
+      header: true, // Include headers in the CSV file
+    });
+
     res.setHeader('Content-Type', 'text/csv');
     res.setHeader('Content-Disposition', 'attachment; filename=data.csv');
+    console.log('data recieved and now being sent to download')
+    console.log(data)
   
     // Pipe the CSV content to the response
-    res.send(csvData);
+    res.send(data);
   });
   
   app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Server is running at http://localhost:${port}/download-csv`);
 });
 
