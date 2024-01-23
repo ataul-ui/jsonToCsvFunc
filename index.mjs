f5d70616d98f4de61ad239e071799cdf64291287
Ataulhaq Akbar <ataul.akbar@gmail.com>
2024-01-23 16:44:36 -0500

    cleaned up and added consise comments

--- index.mjs
+++ index.mjs
@@ -1,12 +1,10 @@
 
-//import data from './sample_data/data.json' assert { type: 'json' };
 import Papa from "papaparse";
 import express from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
 import createCsvWriter from 'csv-writer';
 
-import fs from 'fs/promises'; // Use fs.promises for async file operations
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -17,40 +15,20 @@ const app = express();
 const port = 3000;
 
 
-
-// Convert JSON to CSV
-//const csvData = Papa.unparse(data.Bars, {
-//    header: true, // Include headers in the CSV file
-//});
-
 app.use(bodyParser.json());
+
 
 app.post('/download-csv', async (req, res) => {
     try {
-        //make sure req body is defined
-        if (!req.body || !req.body.Bars || !Array.isArray(req.body.Bars)) {
-            throw new Error('Invalid JSON structure.');
-        }
 
+        // This configures the payload in an acceptable format for the rest of the code
+        // IF YOU'VE MADE CHANGES TO THE POSTMAN CODE, YOU'LL NEED TO CHANGE THIS AS WELL
         const jsonData = req.body.Bars;
 
-        //console.log('Received JSON data:', jsonData);
-
+        //This checks if the incoming json payload is in the proper format
         if (!jsonData || jsonData.length === 0) {
             throw new Error('JSON data is empty or not an array.');
         }
-
-        /*
-        // Convert JSON to CSV
-        const csvData = Papa.unparse(jsonData, {
-            header: true, // Include headers in the CSV file
-        });
-        */
-
-        // Log the generated CSV data
-        //console.log('Generated CSV data:', csvData);
-
-        
 
         // Set response headers for CSV file download
         res.setHeader('Content-Type', 'text/csv');
@@ -69,8 +47,6 @@ app.post('/download-csv', async (req, res) => {
         // Write the JSON data to the CSV file
         await csvWriter.writeRecords(jsonData);
 
-        // Write the CSV content to the file
-        //await fs.writeFile(filePath, csvData, 'utf8');
 
         //fs.writeFileSync(filePath, csvData, 'utf8');
         console.log('CSV file saved successfully:', filePath);
@@ -80,11 +56,6 @@ app.post('/download-csv', async (req, res) => {
         res.status(200).send('check the data folder')
 
 
-        /*
-        console.log(jsonData)
-        res.status(200).send('recieved the json payload')
-
-        */
 
 
 
